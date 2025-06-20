f8661d56392bc3e2823ec9470e66e34c31f40f02
Dalsania <81883338+DalsaniaCode@users.noreply.github.com>
2025-06-19 20:17:37 -0500

    Update backSite.js

--- Javascript/backSite.js
+++ Javascript/backSite.js
@@ -1,4 +1,12 @@
-var site = document.getElementById("backSite");
-site.addEventListener("click", function() {
-  window.location.href = "https://dalsaniacode.github.io/www.dalsantv.ds";
+document.addEventListener('DOMContentLoaded', function() {
+    // Cargando Contenido antes de la ejecución
+    const site = document.getElementById("backSite");
+    // abajo, confirma que funcione bién
+    if (site) {
+        site.addEventListener("click", function() {
+            window.location.href = "https://dalsaniacode.github.io/www.dalsantv.ds";
+        });
+    } else {
+        alert("ERROR: Button for back to home will not work")
+    }
 });
